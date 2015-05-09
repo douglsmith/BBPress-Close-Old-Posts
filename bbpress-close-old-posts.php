10556f4129b8a4ce0f2fca8635ce0f4bdf9fac5c
Doug Smith <douglsmith@users.noreply.github.com>
2015-05-09 10:11:26 -0500

    Direct access protection

--- bbpress-close-old-posts.php
+++ bbpress-close-old-posts.php
@@ -14,6 +14,7 @@ This program is distributed in the hope that it will be useful, but WITHOUT ANY 
 You should have received a copy of the GNU General Public License along with this program; if not, write to the Free Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 */ 
 
+defined('WPINC') or die; // Exit if accessed directly.
 
 register_activation_hook(__FILE__, 'bbpress_topic_scheduler');
 
